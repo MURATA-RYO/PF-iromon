98356a6ecea08743ec994a89fbb152d690f948bb
MURATA-RYO <scincoidae@gmail.com>
2021-09-09 09:57:01 +0000

    投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   
-  resources :items
+  resources :items do
+    resources :comment
+  end  
 end
 
